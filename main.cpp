98aca34a9f2bfbcf76532f5b841c8ab69e1a88bd
kvasudeva24 <vasudevakartik@gmail.com>
2025-12-23 16:53:42 -0600

    need to add remove and destrcutor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "map.hpp"
+#include <algorithm>
 
 
 
@@ -6,14 +7,10 @@ int main(){
 
 
     unordered_map<int, char> test;
-    // test.insert(4, 'c');
-    // int x = 412;
-    // test.insert(x, 'o');
-    // test.insert(11, 'k');
-    // test.insert(22, 'i');
-
     auto array = test.data_;
-    for(int i{}; i<10; i++){
+    test.insert(4, 'c'); 
+    // test.insert(4, 'w');
+    for(size_t i{}; i<test.cap_; i++){
         auto* iter = array[i];
         if(!iter){
             std::cout << "no data on index " << i << std::endl; 
@@ -23,8 +20,58 @@ int main(){
                 iter = iter->next;
             }
         }
-        
     }
+
+
+
+    // for(size_t i{}; i<test.cap_; i++){
+    //     auto* iter = array[i];
+    //     if(!iter){
+    //         std::cout << "no data on index " << i << std::endl; 
+    //     } else {
+    //         while(iter){
+    //             std::cout << "Index: " << i << " --- Key/Value: " << iter->key << "/" << iter->value << std::endl;
+    //             iter = iter->next;
+    //         }
+    //     }
+    // }
+
+    // test.insert(4, 'l');
+    // for(size_t i{}; i<test.cap_; i++){
+    //     auto* iter = array[i];
+    //     if(!iter){
+    //         std::cout << "no data on index " << i << std::endl; 
+    //     } else {
+    //         while(iter){
+    //             std::cout << "Index: " << i << " --- Key/Value: " << iter->key << "/" << iter->value << std::endl;
+    //             iter = iter->next;
+    //         }
+    //     }
+    // }
+
+    // std::cout << std::addressof(array) << std::endl;
+    // std::cout << test.cap_ << std::endl;
+
+
+    // for(size_t i{}; i<test.cap_; i++){
+    //     auto* iter = array[i];
+    //     if(!iter){
+    //         std::cout << "no data on index " << i << std::endl; 
+    //     } else {
+    //         while(iter){
+    //             std::cout << "Index: " << i << " --- Key/Value: " << iter->key << "/" << iter->value << std::endl;
+    //             iter = iter->next;
+    //         }
+    //     }
+    // }
 
     return 0;
 }
+
+
+
+
+// int x = 412;
+    // test.insert(x, 'o');
+    // test.insert(11, 'k');
+    // test.insert(22, 'i');
--- map.hpp
+++ map.hpp
@@ -18,70 +18,75 @@ class unordered_map{
 public: //public for debugging, private actually
     Node** data_ = nullptr;
     size_t size_ = 0;
-    size_t cap_ = 10;
+    size_t cap_ = 3;
     static constexpr float alpha = 0.7;
 
     void resize(){
-        if(size_ * alpha >= cap_){
-            Node** newData = new Node*[cap_ * 3];
-            for(size_t i{}; i<cap_; i++){
-                Node* iter = data_[i];
-                if(iter == nullptr) continue;
-            
-                Node* trail = nullptr;
+        size_t new_cap = cap_ * 3;
+        Node** newData = new Node*[new_cap]{};
+        // std::cout << newData << std::endl;
+        // std::cout << data_ << std::endl;
+        for(size_t i{}; i<cap_; i++){
+            Node* item = data_[i];
+            while(item){
+                Node* old = item;
+                item = item->next;
 
-                Node* newNode = new Node();
-                newNode->key = iter->key;
-                newNode->value = iter->value;
+                old->next = nullptr;
 
-
-                //finish
-
-
+                std::size_t hash_key = std::hash<K>{}(old->key);
+                hash_key %= new_cap;
+                Node* iter = newData[hash_key];
+                if(iter == nullptr){
+                    newData[hash_key] = old;
+                } else {
+                    old->next = newData[hash_key];
+                    newData[hash_key] = old;
+                } 
             }
-
-
-
         }
-    }
+        cap_ = new_cap;
+        delete[] data_;
+        data_ = newData;
+        // std::cout << data_ << std::endl;
+}
 
 public:
     unordered_map(){
-        data_ = new Node*[10];
-        std::cout << "Hello world" << std::endl;
+        data_ = new Node*[cap_]{};
     }
 
 
-    void insert(K key, V value){
+    void insert(K key, V value) {
+    
+        if (size_ >= cap_ * alpha) {
+            resize();
+        }
+
+        std::size_t hash_key = std::hash<K>{}(key) % cap_;
+        Node* curr = data_[hash_key];
+
+        
+        while (curr != nullptr) {
+            if (curr->key == key) {
+                curr->value = value; 
+                return;              
+            }
+            curr = curr->next;
+        }
+
         Node* newNode = new Node();
         newNode->key = key;
         newNode->value = value;
 
-        std::size_t hash_key = std::hash<K>{}(key);
-        hash_key %= cap_;
-
-        Node*& iter = data_[hash_key];
+        newNode->next = data_[hash_key]; 
+        data_[hash_key] = newNode;       
         
-        if(iter == nullptr){
-            iter = newNode;
-        } else {
-            Node* curr = iter;
-            while(curr->next){
-                if(curr->key == key && curr->value == value){
-                    delete newNode;
-                    return;
-                }
-                curr = curr->next;
-            }
-            if(curr->key == key && curr->value == value){
-                delete newNode;
-                return;
-            }
-            curr->next = newNode;
-        }
         size_++;
-        resize();
     }
+
+
+
 
     size_t size() const { return size_; };
     size_t capacity() const { return cap_; }; 
